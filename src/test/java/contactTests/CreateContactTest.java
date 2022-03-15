5805900c8848cdf416b5fc2d1006b252e49aab23
Kavyashree <skavyashree96@gmail.com>
2022-03-15 13:17:26 +0530

    Practice commit

---
+++ src/test/java/contactTests/CreateContactTest.java
@@ -0,0 +1,14 @@
+package contactTests;
+
+import org.testng.annotations.Test;
+
+public class CreateContactTest {
+	
+			
+		@Test
+		public void contact()
+		{
+		System.out.println("contact created");
+	}
+
+}
---
+++ src/test/java/organizationTests/CreateOrgTest.java
@@ -0,0 +1,18 @@
+package organizationTests;
+
+import org.testng.annotations.Test;
+
+public class CreateOrgTest {
+	
+	@Test
+	public void createorg()
+	{
+		System.out.println("organization is created");
+	}
+
+	@Test
+	public void createOrg1()
+	{
+		System.out.println("organization with mandatory field");
+	}
+}
